033dc796333e2fb21417e73d02ed23f8dbc8ed0a
Gustavo <luisgustavolg1033@gmail.com>
2020-11-30 20:28:19 -0300

    initial create

---
+++ android/app/src/main/java/com/example/loja_virtual/loja_virtual/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.loja_virtual.loja_virtual;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/tabs/home_tab.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class HomeTab extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Widget _buildBodyBack() => Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 211, 118, 130),
+                Color.fromARGB(255, 253, 181, 168),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+        );
+
+    return Stack(
+      children: <Widget>[
+        _buildBodyBack(),
+        CustomScrollView(
+          slivers: <Widget>[
+            SliverAppBar(
+              floating: true,
+              snap: true,
+              backgroundColor: Colors.transparent,
+              elevation: 0.0,
+              flexibleSpace: FlexibleSpaceBar(
+                title: const Text("Novidades"),
+                centerTitle: true,
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
