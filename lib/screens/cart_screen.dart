5a73f9aba2688171457d0da4aff29660967569de
Gustavo <luisgustavolg1033@gmail.com>
2021-01-06 20:13:25 -0300

    (feat) added discout card and snack bar to valid and invalid coupom

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -3,6 +3,7 @@ import 'package:loja_virtual/models/cart_model.dart';
 import 'package:loja_virtual/models/user_model.dart';
 import 'package:loja_virtual/screens/login_screen.dart';
 import 'package:loja_virtual/tiles/cart_tile.dart';
+import 'package:loja_virtual/widgets/discount_card.dart';
 import 'package:scoped_model/scoped_model.dart';
 
 class CartScreen extends StatelessWidget {
@@ -85,6 +86,7 @@ class CartScreen extends StatelessWidget {
                   return CartTile(product);
                 }).toList(),
               ),
+              DiscountCard(),
             ],
           );
         }
