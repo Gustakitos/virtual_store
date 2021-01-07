95ba93e36d82487ea54b623dfcbfa4a73d20b2b6
Gustavo <luisgustavolg1033@gmail.com>
2021-01-06 21:14:08 -0300

    (feat) added order with products to database

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -2,8 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:loja_virtual/models/cart_model.dart';
 import 'package:loja_virtual/models/user_model.dart';
 import 'package:loja_virtual/screens/login_screen.dart';
+import 'package:loja_virtual/screens/orders_screen.dart';
 import 'package:loja_virtual/tiles/cart_tile.dart';
+import 'package:loja_virtual/widgets/cart_price.dart';
 import 'package:loja_virtual/widgets/discount_card.dart';
+import 'package:loja_virtual/widgets/ship_card.dart';
 import 'package:scoped_model/scoped_model.dart';
 
 class CartScreen extends StatelessWidget {
@@ -87,6 +90,19 @@ class CartScreen extends StatelessWidget {
                 }).toList(),
               ),
               DiscountCard(),
+              ShipCard(),
+              CartPrice(() async {
+                String orderId = await model.finishOrder();
+
+                if (orderId != null) {
+                  print("orderId: " + orderId);
+                  Navigator.of(context).pushReplacement(
+                    MaterialPageRoute(
+                      builder: (context) => OrderScreen(orderId),
+                    ),
+                  );
+                }
+              }),
             ],
           );
         }
